9ba8b69ca20e4e51107ef5ec01390de6bad2048e
maria matarazzo <mclarapmatarazzo@gmail.com>
2024-08-21 16:22:24 -0300

    Aula dia 21 de Agosto

--- Fornecedor.java
+++ Fornecedor.java
@@ -1,5 +1,70 @@
 package Aula_21_08;
 
 public class Fornecedor {
+	int id;
+	String razaoSocial;
+	String cnpj;
+	String endereco;
+	String telefone;
+	String email;
+	String inscricaoEstadual;
+
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setrazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getcnpj() {
+		return cnpj;
+	}
+	public void setcnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getendereco() {
+		return endereco;
+	}
+	public void setendereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String gettelefone() {
+		return telefone;
+	}
+	public void settelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getemail() {
+		return email;
+	}
+	public void setemail(String email) {
+		this.email = email;
+	}
+	public String getinscricaoEstadual() {
+		return inscricaoEstadual;
+	}
+	public void setinscricaoEstadual(String inscricaoEstadual) {
+		this.inscricaoEstadual = inscricaoEstadual;
+	}
+
+	public String Entregar() {
+		return "Entregar";
+	}
+	public String Agenda() {
+		return "Agendar";
+	}
+	public String venda() {
+		return "vender";
+	}
+
+
+
 
 }
+
--- ObjetoFornecedor.java
+++ ObjetoFornecedor.java
@@ -5,6 +5,33 @@ public class ObjetoFornecedor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Fornecedor fornecedor = new Fornecedor ();
+
+		fornecedor.setId(8520);
+		fornecedor.setrazaoSocial("Usuario:876543321");
+		fornecedor.setcnpj("8765434");
+		fornecedor.setemail("5286947");
+		fornecedor.setendereco("Navegar");
+		fornecedor.settelefone("Navegar");
+		fornecedor.setinscricaoEstadual("Navegar");
+
+
+
+		System.out.println(fornecedor.getRazaoSocial());
+		System.out.println(fornecedor.getcnpj());
+		System.out.println(fornecedor.getId());
+		System.out.println(fornecedor.getemail());
+		System.out.println(fornecedor.getendereco());
+		System.out.println(fornecedor.gettelefone());
+		System.out.println(fornecedor.getinscricaoEstadual());
+
+		System.out.println(fornecedor.Entregar());
+		System.out.println(fornecedor.venda());
+		System.out.println(fornecedor.Agenda());
+
+
+
+
 	}
 
 }
